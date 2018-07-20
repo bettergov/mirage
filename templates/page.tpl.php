5958ee62e9f39ad0d35f1f9167268a9920be4e92
Patrick Judge <pjudge@bettergov.org>
2018-07-20 10:58:02 -0500

    refactor page template into partials

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -8,45 +8,7 @@
  */
 ?>
 
-<header class="header" role="banner">
-
-  <?php if ($logo): ?>
-    <a href="<?php print $front_page;?>" title="<?php print t('Home');?>" rel="home" class="header__logo"><img src="<?php print $logo;?>" alt="<?php print t('Home');?>" class="header__logo-image" /></a>
-  <?php endif;?>
-
-  <?php if ($site_name || $site_slogan): ?>
-    <div class="header__name-and-slogan">
-      <?php if ($site_name): ?>
-        <h1 class="header__site-name">
-          <a href="<?php print $front_page;?>" title="<?php print t('Home');?>" class="header__site-link" rel="home"><span><?php print $site_name;?></span></a>
-        </h1>
-      <?php endif;?>
-
-      <?php if ($site_slogan): ?>
-        <div class="header__site-slogan"><?php print $site_slogan;?></div>
-      <?php endif;?>
-    </div>
-  <?php endif;?>
-
-  <?php if ($secondary_menu): ?>
-    <nav class="header__secondary-menu" role="navigation">
-      <?php print theme('links__system_secondary_menu', array(
-    'links' => $secondary_menu,
-    'attributes' => array(
-        'class' => array('links', 'inline', 'clearfix'),
-    ),
-    'heading' => array(
-        'text' => $secondary_menu_heading,
-        'level' => 'h2',
-        'class' => array('visually-hidden'),
-    ),
-));?>
-    </nav>
-  <?php endif;?>
-
-  <?php print render($page['header']);?>
-
-</header>
+<?php include 'parts/header.inc.php';?>
 
 <div class="layout-center">
 
@@ -72,53 +34,9 @@ elseif ($sidebar_first):
 endif;
 ?>
 
-    <main class="<?php print $content_class;?>" role="main">
-      <?php print render($page['highlighted']);?>
-      <?php print $breadcrumb;?>
-      <a href="#skip-link" class="visually-hidden visually-hidden--focusable" id="main-content">Back to top</a>
-      <?php print render($title_prefix);?>
-      <?php if (!$node && $title): ?>
-        <h1><?php print $title;?></h1>
-      <?php endif;?>
-      <?php print render($title_suffix);?>
-      <?php print $messages;?>
-      <?php print render($tabs);?>
-      <?php print render($page['help']);?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links);?></ul>
-      <?php endif;?>
-      <?php print render($page['content']);?>
-      <?php print $feed_icons;?>
-    </main>
+    <?php include 'parts/content.inc.php';?>
 
-    <div class="layout-swap__top layout-3col__full">
-
-      <a href="#skip-link" class="visually-hidden visually-hidden--focusable" id="main-menu" tabindex="-1">Back to top</a>
-
-      <?php if ($main_menu): ?>
-        <nav class="main-menu" role="navigation">
-          <?php
-// This code snippet is hard to modify. We recommend turning off the
-// "Main menu" on your sub-theme's settings form, deleting this PHP
-// code block, and, instead, using the "Menu block" module.
-// @see https://drupal.org/project/menu_block
-print theme('links__system_main_menu', array(
-    'links' => $main_menu,
-    'attributes' => array(
-        'class' => array('navbar', 'clearfix'),
-    ),
-    'heading' => array(
-        'text' => t('Main menu'),
-        'level' => 'h2',
-        'class' => array('visually-hidden'),
-    ),
-));?>
-        </nav>
-      <?php endif;?>
-
-      <?php print render($page['navigation']);?>
-
-    </div>
+    <?php //include 'parts/main-menu.inc.php';?>
 
     <?php if ($sidebar_first): ?>
       <aside class="<?php print $sidebar_first_class;?>" role="complementary">
